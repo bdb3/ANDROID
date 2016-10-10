42d42e7b286b7924fe0d2f154d9feea7d88b9f74
nikkodg <nikkdg@gmail.com>
2016-10-09 23:44:47 -0700

    updated
    updated inital gui

--- app/src/main/java/com/example/edwin/photoarchive/Activity2.java
+++ app/src/main/java/com/example/edwin/photoarchive/Activity2.java
@@ -3,16 +3,26 @@ package com.example.edwin.photoarchive;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import com.roughike.bottombar.BottomBar;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Activity2 extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
 
-        TextView name=(TextView)findViewById(R.id.textView4);
+        //TextView name=(TextView)findViewById(R.id.textView4);
         //name.setText("Hello "+getIntent().getExtras().getString("userName") );
+
+
 
     }
 }
