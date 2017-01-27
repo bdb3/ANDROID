a237242c1c5a86f8965f070ca23884272f5eff8e
eochoa5 <edwinochoa57@gmail.com>
2017-01-26 21:09:14 -0800

    fixed app crashing on Android version 6 and up, started implementation of upload history section

--- PhotoArchive/app/src/main/java/com/example/edwin/photoarchive/TabFragment4.java
+++ PhotoArchive/app/src/main/java/com/example/edwin/photoarchive/TabFragment4.java
@@ -1,15 +1,72 @@
 package com.example.edwin.photoarchive;
 
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 public class TabFragment4 extends Fragment {
+    private Context context = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.tab_fragment_4, container, false);
+        context= this.getContext();
+        View view = inflater.inflate(R.layout.tab_fragment_4, container, false);
+
+        final LinearLayout historyContainer = (LinearLayout) view.findViewById(R.id.historyContainer);
+
+        // image containers
+
+        for(int i= 0; i<11; i++) {
+
+            LinearLayout imageContainer = new LinearLayout(context);
+            imageContainer.setOrientation(LinearLayout.HORIZONTAL);
+            LinearLayout.LayoutParams layoutParams1 = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            layoutParams1.setMargins(0,15,0,0);
+
+            ImageView imageView = new ImageView(context);
+            imageView.setLayoutParams(new GridView.LayoutParams(150, 150));
+            imageView.setBackgroundColor(Color.BLUE);
+            imageContainer.addView(imageView);
+
+            LinearLayout imageInfoContainer = new LinearLayout(context);
+            imageInfoContainer.setOrientation(LinearLayout.VERTICAL);
+            LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+                    LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+            layoutParams.setMargins(10, 0, 0, 0);
+
+            TextView textView = new TextView(context);
+            textView.setText("Department: Meeting");
+            imageInfoContainer.addView(textView);
+
+            TextView textView1 = new TextView(context);
+            textView1.setText("Upload date: 1/23/17");
+            imageInfoContainer.addView(textView1);
+
+            TextView textView2 = new TextView(context);
+            textView2.setText("Details: ");
+            imageInfoContainer.addView(textView2);
+
+            imageContainer.addView(imageInfoContainer, layoutParams);
+
+            historyContainer.addView(imageContainer, layoutParams1);
+
+        }
+
+
+
+        return view;
     }
 }
